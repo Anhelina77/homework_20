0784221783924f915ad7112ceb09f451f51b5021
Anhelina77 <angelinacheban989@gmail.com>
2022-07-14 18:44:34 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-class Todo {
+class TodoList {
     constructor() {
-        this.todoBox = document.createElement('div');
         this.list = [];
     }
 
@@ -43,15 +42,229 @@ class Todo {
             performing: all - completed,
         };
     };
+}
 
-    updateInfo = () => {
-        const info = this.info();
-        const all = document.querySelector('#all');
-        const completed = document.querySelector('#completed');
-        const performing = document.querySelector('#performing');
-        all.innerText = 'all: ' + info.all;
-        completed.innerText = 'completed: ' + info.completed;
-        performing.innerText = 'performing: ' + info.performing;
+class TodoInfo {
+    constructor(info) {
+        this.all = info.all;
+        this.completed = info.completed;
+        this.performing = info.performing;
+    }
+
+    update = info => {
+        this.all = info.all;
+        this.completed = info.completed;
+        this.performing = info.performing;
+
+        const all = this.container.querySelector('#all');
+        const completed = this.container.querySelector('#completed');
+        const performing = this.container.querySelector('#performing');
+        all.innerText = 'all: ' + this.all;
+        completed.innerText = 'completed: ' + this.completed;
+        performing.innerText = 'performing: ' + this.performing;
+    };
+
+    render = () => {
+        this.container = document.createElement('div');
+        const all = document.createElement('div');
+        all.id = 'all';
+        all.innerText = 'all: ' + this.all;
+
+        const completed = document.createElement('div');
+        completed.id = 'completed';
+        completed.innerText = 'completed: ' + this.completed;
+
+        const performing = document.createElement('div');
+        performing.id = 'performing';
+        performing.innerText = 'performing: ' + this.performing;
+
+        this.container.innerHTML = ''; // reset
+        this.container.appendChild(all);
+        this.container.appendChild(completed);
+        this.container.appendChild(performing);
+
+        return this.container;
+    };
+}
+
+class TodoCreate {
+    constructor(createTask) {
+        this.createTask = createTask;
+    }
+
+    renderTitle = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const title = document.createElement('input');
+        title.setAttribute('required', '');
+        title.setAttribute('maxlength', '10');
+        title.classList.add('title');
+        ceil.appendChild(title);
+
+        return ceil;
+    };
+
+    renderText = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const text = document.createElement('input');
+        text.setAttribute('required', '');
+        text.classList.add('text');
+        ceil.appendChild(text);
+
+        return ceil;
+    };
+
+    renderAdd = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const add = document.createElement('button');
+        add.type = 'submit';
+        add.innerText = 'Add task';
+        ceil.appendChild(add);
+
+        return ceil;
+    };
+
+    renderTable = () => {
+        const table = document.createElement('table');
+        table.setAttribute('width', '100%');
+        table.setAttribute('height', '100px');
+        const row = document.createElement('tr');
+        row.appendChild(this.renderTitle());
+        const last = document.createElement('td');
+
+        table.appendChild(this.renderTitle());
+        table.appendChild(this.renderText());
+        table.appendChild(this.renderAdd());
+        table.appendChild(last);
+
+        return table;
+    };
+
+    render = () => {
+        this.container = document.createElement('form');
+        this.container.setAttribute('action', '#');
+        this.container.addEventListener('submit', this.createTask);
+
+        this.container.innerHTML = ''; // reset
+        this.container.appendChild(this.renderTable());
+
+        return this.container;
+    };
+}
+
+class TodoItem {
+    constructor(task, saveTask, removeTask) {
+        this.task = task;
+        this.saveTask = saveTask;
+        this.removeTask = removeTask;
+    }
+
+    renderTitle = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '10%');
+        const title = document.createElement('span');
+        title.classList.add('title');
+        title.innerText = this.task.title;
+        ceil.appendChild(title);
+        return ceil;
+    };
+
+    renderText = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const text = document.createElement('input');
+        text.setAttribute('required', '');
+        text.classList.add('text');
+        text.value = this.task.text;
+        ceil.appendChild(text);
+
+        return ceil;
+    };
+
+    renderCompleted = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '5%');
+        const completed = document.createElement('input');
+        completed.classList.add('completed');
+        completed.type = 'checkbox';
+        completed.checked = this.task.completed;
+        ceil.appendChild(completed);
+
+        return ceil;
+    };
+
+    renderSave = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const save = document.createElement('button');
+        save.type = 'submit';
+        save.innerText = 'Save task';
+        ceil.appendChild(save);
+
+        return ceil;
+    };
+
+    renderRemove = () => {
+        const ceil = document.createElement('td');
+        ceil.setAttribute('width', '20%');
+        const remove = document.createElement('button');
+        remove.addEventListener('click', this.removeTask.bind(null, this.task, this.container));
+        remove.type = 'button';
+        remove.innerText = 'Remove task';
+        ceil.appendChild(remove);
+
+        return ceil;
+    };
+
+    renderTable = () => {
+        const table = document.createElement('table');
+        table.setAttribute('width', '100%');
+        const row = document.createElement('tr');
+        row.appendChild(this.renderTitle());
+        const last = document.createElement('td');
+
+        table.appendChild(this.renderTitle());
+        table.appendChild(this.renderText());
+        table.appendChild(this.renderCompleted());
+        table.appendChild(this.renderSave());
+        table.appendChild(this.renderRemove());
+        table.appendChild(last);
+
+        return table;
+    };
+
+    render = () => {
+        this.container = document.createElement('form');
+        this.container.setAttribute('action', '#');
+        this.container.addEventListener('submit', this.saveTask);
+
+        this.container.innerHTML = ''; // reset
+        this.container.appendChild(this.renderTable());
+
+        return this.container;
+    };
+}
+
+class Todo {
+    constructor() {
+        this.todo = new TodoList();
+    }
+
+    createTask = e => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const task = {
+            title: form.querySelector('.title').value,
+            text: form.querySelector('.text').value,
+            completed: false,
+        };
+        if (this.todo.addTask(task)) {
+            const item = new TodoItem(task, this.saveTask, this.removeTask);
+            this.container.appendChild(item.render());
+            this.infoContainer.update(this.todo.info());
+        }
     };
 
     saveTask = e => {
@@ -62,171 +275,39 @@ class Todo {
             text: form.querySelector('.text').value,
             completed: form.querySelector('.completed').checked,
         };
-        if (this.editTask(task)) {
-            this.updateInfo();
+        if (this.todo.editTask(task)) {
+            this.infoContainer.update(this.todo.info());
         }
     };
 
-    onRemoveTask = (task, todoItem) => {
-        if (this.removeTask(task)) {
-            this.todoBox.removeChild(todoItem);
-            this.updateInfo();
+    removeTask = (task, todoItem) => {
+        if (this.todo.removeTask(task)) {
+            this.container.removeChild(todoItem);
+            this.infoContainer.update(this.todo.info());
         }
     };
 
-    renderItem = item => {
-        const todoItem = document.createElement('form');
-        todoItem.setAttribute('action', '#');
-        todoItem.addEventListener('submit', this.saveTask);
+    render = () => {
+        this.container = document.createElement('div');
+        this.infoContainer = new TodoInfo(this.todo.info());
+        this.createContainer = new TodoCreate(this.createTask);
 
-        const table = document.createElement('table');
-        table.setAttribute('width', '100%');
-        const row = document.createElement('tr');
-
-        const ceil1 = document.createElement('td');
-        ceil1.setAttribute('width', '10%');
-        const title = document.createElement('span');
-        title.classList.add('title');
-        title.innerText = item.title;
-        ceil1.appendChild(title);
-
-        const ceil2 = document.createElement('td');
-        ceil2.setAttribute('width', '20%');
-        const text = document.createElement('input');
-        text.setAttribute('required', '');
-        text.classList.add('text');
-        text.value = item.text;
-        ceil2.appendChild(text);
-
-        const ceil3 = document.createElement('td');
-        ceil3.setAttribute('width', '5%');
-        const completed = document.createElement('input');
-        completed.classList.add('completed');
-        completed.type = 'checkbox';
-        completed.checked = item.completed;
-        ceil3.appendChild(completed);
-
-        const ceil4 = document.createElement('td');
-        ceil4.setAttribute('width', '20%');
-        const save = document.createElement('button');
-        save.type = 'submit';
-        save.innerText = 'Save task';
-        ceil4.appendChild(save);
-
-        const ceil5 = document.createElement('td');
-        ceil5.setAttribute('width', '20%');
-        const remove = document.createElement('button');
-        remove.addEventListener('click', this.onRemoveTask.bind(null, item, todoItem));
-        remove.type = 'button';
-        remove.innerText = 'Remove task';
-        ceil5.appendChild(remove);
-
-        const ceil6 = document.createElement('td');
-
-        row.appendChild(ceil1);
-        row.appendChild(ceil2);
-        row.appendChild(ceil3);
-        row.appendChild(ceil4);
-        row.appendChild(ceil5);
-        row.appendChild(ceil6);
-        table.appendChild(row);
-        todoItem.appendChild(table);
-
-        return todoItem;
-    };
-
-    createTask = e => {
-        e.preventDefault();
-        const form = e.currentTarget;
-        const task = {
-            title: form.querySelector('.title').value,
-            text: form.querySelector('.text').value,
-            completed: false,
-        };
-        if (this.addTask(task)) {
-            this.todoBox.appendChild(this.renderItem(task));
-            this.updateInfo();
-        }
-    };
-
-    renderCreate = () => {
-        const todoItem = document.createElement('form');
-        todoItem.setAttribute('action', '#');
-        todoItem.addEventListener('submit', this.createTask);
-
-        const table = document.createElement('table');
-        table.setAttribute('width', '100%');
-        table.setAttribute('height', '100px');
-        const row = document.createElement('tr');
-
-        const ceil1 = document.createElement('td');
-        ceil1.setAttribute('width', '20%');
-        const title = document.createElement('input');
-        title.setAttribute('required', '');
-        title.setAttribute('maxlength', '10');
-        title.classList.add('title');
-        ceil1.appendChild(title);
-
-        const ceil2 = document.createElement('td');
-        ceil2.setAttribute('width', '20%');
-        const text = document.createElement('input');
-        text.setAttribute('required', '');
-        text.classList.add('text');
-        ceil2.appendChild(text);
-
-        const ceil3 = document.createElement('td');
-        ceil3.setAttribute('width', '20%');
-        const add = document.createElement('button');
-        add.type = 'submit';
-        add.innerText = 'Add task';
-        ceil3.appendChild(add);
-
-        const ceil4 = document.createElement('td');
-
-        row.appendChild(ceil1);
-        row.appendChild(ceil2);
-        row.appendChild(ceil3);
-        row.appendChild(ceil4);
-        table.appendChild(row);
-        todoItem.appendChild(table);
-
-        return todoItem;
-    };
-
-    renderInfo = () => {
-        const infoBox = document.createElement('div');
-
-        const all = document.createElement('div');
-        all.id = 'all';
-
-        const completed = document.createElement('div');
-        completed.id = 'completed';
-
-        const performing = document.createElement('div');
-        performing.id = 'performing';
-
-        infoBox.appendChild(all);
-        infoBox.appendChild(completed);
-        infoBox.appendChild(performing);
-
-        return infoBox;
-    };
-
-    render = container => {
-        this.list.forEach(item => {
-            this.todoBox.appendChild(this.renderItem(item));
+        this.container.innerHTML = ''; // reset
+        this.container.appendChild(this.infoContainer.render());
+        this.container.appendChild(this.createContainer.render());
+        this.todo.list.forEach(task => {
+            const item = new TodoItem(task, this.saveTask, this.removeTask);
+            this.container.appendChild(item.render());
         });
-        container.prepend(this.todoBox);
-        container.prepend(this.renderCreate());
-        container.prepend(this.renderInfo());
-        this.updateInfo();
+
+        return this.container;
     };
 }
 
-const todo = new Todo();
+const developers = new Todo();
 
-todo.addTask({ title: 'task1', text: 'text1', completed: false });
-todo.addTask({ title: 'task2', text: 'text2', completed: false });
-todo.addTask({ title: 'task3', text: 'text3', completed: false });
+developers.todo.addTask({ title: 'task1', text: 'text1', completed: false });
+developers.todo.addTask({ title: 'task2', text: 'text2', completed: false });
+developers.todo.addTask({ title: 'task3', text: 'text3', completed: false });
 
-todo.render(document.body);
+document.body.prepend(developers.render());
